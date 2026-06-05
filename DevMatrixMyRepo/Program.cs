b60aaac052782a0a05d83ee26b7e144206d316be
Odusina Tolulope <odusinat8@gmail.com>
2026-06-06 00:33:39 +0100

    operators

--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,30 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DevMatrixMyRepo;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-for (int i = 0; i <= 20; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
-
-
-string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
-foreach (string name in names)
-{
-    Console.WriteLine(name);
-}
+//for (int i = 0; i <= 20; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
 
 
+//string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
+//foreach (string name in names)
+//{
+//    Console.WriteLine(name);
+//}
 
-UserDetails user = new UserDetails(1001, "Odusina Tolulope ", 25);
-user.UserInfo();
 
-StudentsDetails students = new StudentsDetails("Odusina", "Tolulope", 30, "Computer Science", "400");
-students.StudentInfo();
 
-DataTypes data = new DataTypes();
-data.DataTypesInfo();
+//UserDetails user = new UserDetails(1001, "Odusina Tolulope ", 25);
+//user.UserInfo();
+
+//StudentsDetails students = new StudentsDetails("Odusina", "Tolulope", 30, "Computer Science", "400");
+//students.StudentInfo();
+
+//DataTypes data = new DataTypes();
+//data.DataTypesInfo();
+
+Operators.OperatorsInfo();
