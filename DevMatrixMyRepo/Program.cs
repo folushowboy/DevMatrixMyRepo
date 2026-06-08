f417600a922aa95030f6610078060ba26647a335
Odusina Tolulope <odusinat8@gmail.com>
2026-06-08 20:34:35 +0100

    new update

--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,42 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DevMatrixMyRepo;
 
-//Console.WriteLine("Hello, World!");
-
-//for (int i = 0; i <= 20; i++)
-//{
-//    if (i % 2 == 0)
-//    {
-//        Console.WriteLine(i);
-//    }
-//}
-
-
-//string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
-//foreach (string name in names)
-//{
-//    Console.WriteLine(name);
-//}
+Console.WriteLine("Hello, World!");
 
 
 
-UserDetails user = new UserDetails(1001, "Odusina Tolulope ", 25);
-//user.UserInfo();
+Operators.OperatorsInfo();
 
 
-//StudentsDetails students = new StudentsDetails("Odusina", "Tolulope", 30, "Computer Science", "400");
-//students.StudentInfo();
+GuessGame.GuessGameApp();
 
 
-//DataTypes data = new DataTypes();
-//data.DataTypesInfo();
-
-
-//Operators.OperatorsInfo();
-
-
-//GuessGame.GuessGameApp();
-
-
-//CoffeeApp coffee = new CoffeeApp();
-//coffee.CoffeeGame();
+CoffeeApp coffee = new CoffeeApp();
+coffee.CoffeeGame();
