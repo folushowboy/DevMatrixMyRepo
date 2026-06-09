3e2b22d8a3063bbbf94bd4747e1fff27d1eeb00e
Odusina Tolulope <odusinat8@gmail.com>
2026-06-09 16:12:02 +0100

    valueParameter

--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,17 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DevMatrixMyRepo;
+
 Console.WriteLine("Hello, World!");
 
-for (int i = 0; i <= 20; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
-
-
-string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
-foreach (string name in names)
-{
-    Console.WriteLine(name);
-}
+int i = 0;
+ValueParameter.SimpleMethod(i);
+Console.WriteLine(i); //Since it's passing a parameter by value, the result will be 0, cause the value of i is 0. I.e, the result is determined by the value of what is being invoked or called.
---
+++ DevMatrixMyRepo/ValueParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class ValueParameter
+    {
+        public static void SimpleMethod(int j)
+        {
+            j = 20;
+        } 
+    }
+}
