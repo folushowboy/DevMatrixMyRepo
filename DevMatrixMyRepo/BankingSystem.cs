f8923c4b58271308f71cf25214828b1c6f045973
Odusina Tolulope <odusinat8@gmail.com>
2026-06-10 12:26:24 +0100

    BankingSystem

---
+++ DevMatrixMyRepo/BankingSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class BankingSystem
+    {
+        string FullName;
+        string DOB;
+        string Address;
+        long PhoneNumber;
+        string Email;
+
+        public BankingSystem(string fname, string dob, string address, long phonenum, string email)
+        {
+            FullName = fname;
+            DOB = dob;
+            Address = address;
+            PhoneNumber = phonenum;
+            Email = email;
+        }
+        public void CreateAcct()
+        {
+            Console.WriteLine("Account has been created Successfully");
+            Console.WriteLine($"FullName: {FullName}");
+            Console.WriteLine($"Date Of Birth: {DOB}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Phone Number: {PhoneNumber}");
+            Console.WriteLine($"Email: {Email}");
+
+        }
+    }
+}
--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,17 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DevMatrixMyRepo;
 
-for (int i = 0; i <= 20; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
+//Console.WriteLine("Hello, World!");
 
-
-string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
-foreach (string name in names)
-{
-    Console.WriteLine(name);
-}
+BankingSystem bank = new BankingSystem("Odusina Tolulope", "30/01/2000" , "Lagos", 09077867875, "odusinat8@gmail.com");
+bank.CreateAcct();
