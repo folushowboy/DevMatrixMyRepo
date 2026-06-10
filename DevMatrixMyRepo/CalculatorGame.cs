dc932875a8c82f896eacd3e1baa6b2eeac4a0f90
Odusina Tolulope <odusinat8@gmail.com>
2026-06-10 01:24:51 +0100

    StudentCalcGame

---
+++ DevMatrixMyRepo/CalculatorGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class CalculatorGame
+    {
+        string StudentName;
+        int StudentScore;
+
+        public CalculatorGame(string Sname, int Sscore)
+        {
+            StudentName = Sname;
+            StudentScore = Sscore;
+        }
+
+        public void StudentCalcGame()
+        {
+            Console.WriteLine($"Student name is {StudentName}");
+            Console.WriteLine("Student score is {0}", StudentScore);
+
+            if (StudentScore >= 70 && StudentScore <= 100) 
+            {
+                Console.WriteLine("The student score is " + StudentScore + " and it's A");
+            }else if(StudentScore >= 60 && StudentScore <= 69)
+            {
+                Console.WriteLine($"The student score is {StudentScore} and it's B");
+            }
+            else if (StudentScore >= 50 && StudentScore <= 59)
+            {
+                Console.WriteLine($"The student score is {StudentScore} and it's C");
+            }
+            else if (StudentScore >= 45 && StudentScore <= 49)
+            {
+                Console.WriteLine($"The student score is {StudentScore} and it's D");
+            }
+            else if (StudentScore >= 40 && StudentScore <= 45)
+            {
+                Console.WriteLine($"The student score is {StudentScore} and it's E");
+            }
+            else
+            {
+                Console.WriteLine($"The student score is {StudentScore} and it's F");
+            }
+        }
+    }
+}
--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,17 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DevMatrixMyRepo;
+
 Console.WriteLine("Hello, World!");
 
-for (int i = 0; i <= 20; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
-
-
-string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
-foreach (string name in names)
-{
-    Console.WriteLine(name);
-}
+CalculatorGame calcGame = new CalculatorGame("Oke", 33);
+calcGame.StudentCalcGame();
