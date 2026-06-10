f6a9da29a40f663e23df9fb74aa084b36267960a
Odusina Tolulope <odusinat8@gmail.com>
2026-06-10 13:11:14 +0100

    overload & override

---
+++ DevMatrixMyRepo/Calc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class Calc
+    {
+        public static void Add(int a, int b)
+        {
+            var y = a + b;
+            Console.WriteLine(y);
+        }
+        public static void Add(double y, double x, double z)
+        { 
+            var a = y + x + z;
+            Console.WriteLine(a);
+
+        }
+        public static void Add(int x, int y, int z)
+        {
+            var f = y + x + z;
+            Console.WriteLine(f);
+
+        }
+    }
+}
---
+++ DevMatrixMyRepo/MethodOverloading.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    //Method Overloading
+    //Method Overidding
+    internal class MethodOverloading
+    {
+        public virtual void GetStudent()
+        {
+            Console.WriteLine("This is a student class");
+        }
+        public int GetStudent(int v)
+        {
+            return 90;
+        }
+        public string GetStudent(string x, int z)
+        {
+            return "Name";
+        }
+    }
+}
