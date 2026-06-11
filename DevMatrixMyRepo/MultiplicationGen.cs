cd26e406045c1cdf0ac8cad9ab26559cbcc142fd
Odusina Tolulope <odusinat8@gmail.com>
2026-06-12 00:02:47 +0100

    MultiplicationTable

--- DevMatrixMyRepo/MultiplicationGen.cs
+++ DevMatrixMyRepo/MultiplicationGen.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DevMatrixMyRepo
 {
@@ -10,16 +11,31 @@ namespace DevMatrixMyRepo
     {
         public void MultiplyGen()
         {
-            int number = int.Parse(Console.ReadLine());
-            MultiplyGenerate(number);
+            int number;
+            string choice = "YES";
+            while (choice == "YES")
+            {
+                Console.WriteLine("Enter a number.");
+                if (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Invalid input. Enter a number.");
+                    continue;
+                }
+                MultiplyGenerate(number);
+
+                Console.WriteLine("\nGenerate new table? YES/NO");
+                choice = Console.ReadLine().ToUpper();
+
+            }
+
         } 
         public void MultiplyGenerate(int number)
         {
-           for(int i = 1; i <= 12; i++)
+            for(int i = 1; i <= 20; i++)
             {
                 Console.WriteLine($"{number} * {i} = {number * i}");
             }
-          
+         
         }
     }
 }
