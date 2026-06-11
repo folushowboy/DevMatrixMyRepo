231ebdc5c7fcc982534090cf820ad20a892e5dea
Odusina Tolulope <odusinat8@gmail.com>
2026-06-11 22:16:16 +0100

    multiGenerator

---
+++ DevMatrixMyRepo/MultiplicationGen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class MultiplicationGen
+    {
+        int myValue;
+        public void MultiplyGen(int x)
+        {
+            if(!int.TryParse(Console.ReadLine(), out myValue))
+            {
+                Console.WriteLine("Invalid input. Enter a number.");
+            }
+        }
+    }
+}
