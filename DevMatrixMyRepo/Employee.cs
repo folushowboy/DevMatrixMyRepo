7db097f2b84ca2093cbc59202a92f07782eb8b81
Odusina Tolulope <odusinat8@gmail.com>
2026-06-23 12:48:16 +0100

    HiddenMethod

---
+++ DevMatrixMyRepo/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class Employee
+    {
+        string FirstName;
+        string LastName;
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine(FirstName = "Tolulope");
+            Console.WriteLine(LastName = "Odusina");
+        }
+    }
+}
---
+++ DevMatrixMyRepo/FullTimeEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class FullTimeEmployee : Employee
+    {
+        public new void DisplayEmployeeInfo() //The new key word makes the base method hidden and the derived method invoked.
+        {
+            base.DisplayEmployeeInfo();
+            Console.WriteLine("I love yam and egg");
+            Console.WriteLine("Jesus is lord");
+        }
+    }
+}
