8f75d9dbdc6656f77dd29741df6aa39dc7c7a7f1
DELL <DELL@DESKTOP-NPJ4ILO>
2026-06-24 12:59:02 +0100

    delegateLandaExpression

--- DevMatrixMyRepo/DelegateSamples.cs
+++ DevMatrixMyRepo/DelegateSamples.cs
@@ -6,31 +6,99 @@ using System.Threading.Tasks;
 
 namespace DevMatrixMyRepo
 {
-    public delegate void FunctionPointer();
-    public delegate void FunctionPointer2(string x);
-
+    public delegate bool SampleDelegate(DelegateSamples1 students);
     public class DelegateSamples1
     {
-        public static void MySmapleMethod()
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Level { get; set; }
+        
+       public static void DisplayStudentInfo(List<DelegateSamples1> students, SampleDelegate checkLogic)
         {
-            Console.WriteLine("Hello from MySmapleMethod");
-        }
-    }
-    public class DelegateSamples2
-    {
-        public static void MyDelegateMethod()
-        {
-            Console.WriteLine("Hello from DelegateMethod");
-        }
-
-        public static void MyDelegateMethod2(string x)
-        {
-            if (string.IsNullOrEmpty(x)) //Null Collation
+            foreach(DelegateSamples1 std in students)
             {
-                x = "Welcome to the second month of the class";
-
+                if(checkLogic(std))
+                {
+                    std.Level += 100;
+                    Console.WriteLine($"{std.Name} has been promoted to {std.Level} level.");
+                }
             }
-            Console.WriteLine(x);
         }
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public delegate void FunctionPointer();
+    //public delegate void FunctionPointer2(string x);
+
+    //public class DelegateSamples1
+    //{
+    //    public static void MySmapleMethod()
+    //    {
+    //        Console.WriteLine("Hello from MySmapleMethod");
+    //    }
+    //}
+    //public class DelegateSamples2
+    //{
+    //    public static void MyDelegateMethod()
+    //    {
+    //        Console.WriteLine("Hello from DelegateMethod");
+    //    }
+
+    //    public static void MyDelegateMethod2(string x)
+    //    {
+    //        if (string.IsNullOrEmpty(x)) //Null Collation
+    //        {
+    //            x = "Welcome to the second month of the class";
+
+    //        }
+    //        Console.WriteLine(x);
+    //    }
+    //}
 }
--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -2,47 +2,44 @@
 using DevMatrixMyRepo;
 
 //Console.WriteLine("Hello, World!");
-List <DelegateClass> ListOfStudents = new List <DelegateClass> ();
-ListOfStudents.Add(new DelegateClass { Name = "Odusina Tolu", Id = 101, Department = "Chemistry", Level = 100, Age = 10 });
-ListOfStudents.Add(new DelegateClass { Name = "Adedeji Sam", Id = 102, Department = "Biology", Level = 300, Age = 20 });
-ListOfStudents.Add(new DelegateClass { Name = "Aguda Grace", Id = 103, Department = "Physics", Level = 100, Age = 30 });
-ListOfStudents.Add(new DelegateClass { Name = "Afolabi Funke", Id = 104, Department = "Geology", Level = 400, Age = 40 });
-ListOfStudents.Add(new DelegateClass { Name = "Dapo Lawal", Id = 105, Department = "Insurance", Level = 200, Age = 50 });
-ListOfStudents.Add(new DelegateClass { Name = "Oke Mordy", Id = 106, Department = "Elect elect", Level = 500, Age = 70 });
-ListOfStudents.Add(new DelegateClass { Name = "George Flow", Id = 107, Department = "Geography", Level = 100, Age = 60 });
-ListOfStudents.Add(new DelegateClass { Name = "Grace Law", Id = 108, Department = "Acturial Sciences", Level = 200, Age = 80 });
+//List <DelegateClass> ListOfStudents = new List <DelegateClass> ();
+//ListOfStudents.Add(new DelegateClass { Name = "Odusina Tolu", Id = 101, Department = "Chemistry", Level = 100, Age = 10 });
+//ListOfStudents.Add(new DelegateClass { Name = "Adedeji Sam", Id = 102, Department = "Biology", Level = 300, Age = 20 });
+//ListOfStudents.Add(new DelegateClass { Name = "Aguda Grace", Id = 103, Department = "Physics", Level = 100, Age = 30 });
+//ListOfStudents.Add(new DelegateClass { Name = "Afolabi Funke", Id = 104, Department = "Geology", Level = 400, Age = 40 });
+//ListOfStudents.Add(new DelegateClass { Name = "Dapo Lawal", Id = 105, Department = "Insurance", Level = 200, Age = 50 });
+//ListOfStudents.Add(new DelegateClass { Name = "Oke Mordy", Id = 106, Department = "Elect elect", Level = 500, Age = 70 });
+//ListOfStudents.Add(new DelegateClass { Name = "George Flow", Id = 107, Department = "Geography", Level = 100, Age = 60 });
+//ListOfStudents.Add(new DelegateClass { Name = "Grace Law", Id = 108, Department = "Acturial Sciences", Level = 200, Age = 80 });
 
 
-PromoteDeStudentslegate stud = new PromoteDeStudentslegate(PromoteStudents);
-DelegateClass.PromoteStudents(ListOfStudents, stud);
+//PromoteDeStudentslegate stud = new PromoteDeStudentslegate(PromoteStudents);
+//DelegateClass.PromoteStudents(ListOfStudents, stud);
 
-static bool PromoteStudents(DelegateClass students)
-{
-    if(students.Age <= 18)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-
-static bool PromoteStudentsLevel(DelegateClass students)
-{
-    if (students.Level <= 100)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
+//static bool PromoteStudents(DelegateClass students)
+//{
+//    if(students.Age <= 18)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
 
 
-
+//static bool PromoteStudentsLevel(DelegateClass students)
+//{
+//    if (students.Level <= 100)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
 
 
 
@@ -70,32 +67,33 @@ static bool PromoteStudentsLevel(DelegateClass students)
 
 
 
+List<DelegateSamples1> listOfStudents = new List<DelegateSamples1>();
+listOfStudents.Add(new DelegateSamples1 { Name = "Odusina", Age = 20, Level = 100 });
+listOfStudents.Add(new DelegateSamples1 { Name = "Sam", Age = 18, Level = 300 });
+listOfStudents.Add(new DelegateSamples1 { Name = "Tolu", Age = 15, Level = 200 });
+listOfStudents.Add(new DelegateSamples1 { Name = "Mordy", Age = 22, Level = 200 });
+listOfStudents.Add(new DelegateSamples1 { Name = "Emma", Age = 17, Level = 400 });
+
+//SampleDelegate del = new SampleDelegate(IsEligible);
+//DelegateSamples1.DisplayStudentInfo(listOfStudents, del);
+DelegateSamples1.DisplayStudentInfo(listOfStudents, st => st.Age >= 18);
+
+
+//bool IsEligible(DelegateSamples1 students)
+//{
+//    if (students.Age >= 18)
+//    {
+//        return true;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
 
 
 
 
 
 
-
-
-//LIST
-//List<int> listOfValues = new List<int>();
-//listOfValues.Add(1);
-//listOfValues.Add(2);
-//listOfValues.Add(3);
-//listOfValues.Add(4);
-//listOfValues.Add(5);
-//listOfValues.Add(6);
-//listOfValues.Add(7);
-//listOfValues.Add(8);
-
-
-//FunctionPointer2 function = DelegateSamples2.MyDelegateMethod2;
-//function("Welcome");
-//function("Welcome to the class");
-
-//FunctionPointer funct = DelegateSamples1.MySmapleMethod;
-//funct();
-//FunctionPointer func = DelegateSamples2.MyDelegateMethod;
-//func();
 
