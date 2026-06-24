6b8729b1ac24ea8b79e45f207e8e0c4adb1c0267
DELL <DELL@DESKTOP-NPJ4ILO>
2026-06-24 14:04:32 +0100

    smlDocumentation

---
+++ DevMatrixMyRepo/EmployeeData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    /// <summary>
+    /// This class is about a particular institution employee data
+    /// </summary>
+    internal class EmployeeData
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int id {  get; set; }
+
+        /// <summary>
+        /// it is used for checking if a value is deleted
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns>bool</returns>
+        public static bool IsDeleted(string x)
+        {
+            x = "welcome";
+            return true;
+        }
+    }
+}
---
+++ DevMatrixMyRepo/ExceptionStyling.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    //Exception handling is for handling unforeseen errors at runtime
+    public class ExceptionStyling
+    {
+        //This exception is used to check for runtime error and handle it
+
+        public static void MyCalculator()
+        {
+            try
+            {
+                double[] figures = { 3.34, 4.0, 6.7, 54 };
+                Console.WriteLine(figures[8]); // To get the value
+                Console.WriteLine(figures.Length - 1); //To get the last position
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Value not found");
+            }
+            finally
+            {
+
+            }
+        }
+
+        //This exception is used to throw error message
+        public static void ValueCheck()
+        {
+            if(45 > 20)
+            {
+                Console.WriteLine("Age matters");
+            }
+            else
+            {
+                throw new Exception("Age does not matter");
+            }
+        }
+    }
+}
