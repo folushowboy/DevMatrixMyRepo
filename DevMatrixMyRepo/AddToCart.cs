f3a38fb3c0e78b04d3e28e5080345d1c25f3d51d
DELL <DELL@DESKTOP-NPJ4ILO>
2026-06-29 14:27:57 +0100

    AddToCart

---
+++ DevMatrixMyRepo/AddToCart.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class AddToCart
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set;}
+        public int ItemPrice { get; set;}
+
+        public static void ViewProduct(List<AddToCart> items) 
+        {
+            Console.WriteLine("Welcome to the Add-To-Cart website. What would you like to order?");
+            Console.WriteLine("select your order by picking the order number ");
+
+            foreach(AddToCart item in items)
+            {
+                Console.WriteLine($"{item.Id}, {item.ItemName}, {item.ItemPrice}");
+            }
+
+            int price = 0;
+            string coffeName = "";
+            string answer = "YES";
+            while (answer == "YES")
+            {
+                if (!int.TryParse(Console.ReadLine(), out int UserChoice))
+                {
+                    Console.WriteLine("Invalid Input. Must be numbers");
+                    continue;
+                }
+                else if(UserChoice > 10)
+                {
+                    Console.WriteLine("Number is greater than the items we have. Choose between 1 and 10 to choose your order.");
+                }
+               
+                switch (UserChoice)
+                {
+                    case 1:
+                        coffeName = "BlackCoffee";
+                        price = 500;
+                        break;
+                    case 2:
+                        coffeName = "Espresso";
+                        price = 1500;
+                        break;
+                    case 3:
+                        coffeName = "Americano";
+                        price = 6000;
+                        break;
+                    case 4:
+                        coffeName = "Latte";
+                        price = 2000;
+                        break;
+                    case 5:
+                        coffeName = "Cappuccino";
+                        price = 3500;
+                        break;
+                    case 6:
+                        coffeName = "Macchiato";
+                        price = 2550;
+                        break;
+                    case 7:
+                        coffeName = "Mocha";
+                        price = 4250;
+                        break;
+                    case 8:
+                        coffeName = "Flat White ";
+                        price = 1500;
+                        break;
+                    case 9:
+                        coffeName = "Doppio";
+                        price = 2300;
+                        break;
+                    case 10:
+                        coffeName = "Affogato";
+                        price = 3400;
+                        break;
+                }
+                Console.WriteLine($"You picked {coffeName}");
+                Console.WriteLine("Do you still want to pick more item? YES/NO");
+                 answer = Console.ReadLine().ToUpper();
+                //int total = price 
+            }        
+        }
+
+        public static void AddProductToCart(int UserChoice)
+        {
+            Console.WriteLine("Add to cart using the item ID");
+        } 
+    }
+}
--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,17 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DevMatrixMyRepo;
+
 Console.WriteLine("Hello, World!");
 
-for (int i = 0; i <= 20; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(i);
-    }
-}
+List<AddToCart> listOfItems = new List<AddToCart>();
+listOfItems.Add(new AddToCart() { Id = 1, ItemName = "BlackCoffee ", ItemPrice = 1550 });
+listOfItems.Add(new AddToCart() { Id = 2, ItemName = "Espresso ", ItemPrice = 2000 });
+listOfItems.Add(new AddToCart() { Id = 3, ItemName = "Americano ", ItemPrice = 3550 });
+listOfItems.Add(new AddToCart() { Id = 4, ItemName = "Latte ", ItemPrice = 1000 });
+listOfItems.Add(new AddToCart() { Id = 5, ItemName = "Cappuccino ", ItemPrice = 2300 });
+listOfItems.Add(new AddToCart() { Id = 6, ItemName = "Macchiato ", ItemPrice = 5550 });
+listOfItems.Add(new AddToCart() { Id = 7, ItemName = "Mocha ", ItemPrice = 5000 });
+listOfItems.Add(new AddToCart() { Id = 8, ItemName = "Flat White ", ItemPrice = 3300 });
+listOfItems.Add(new AddToCart() { Id = 9, ItemName = "Doppio ", ItemPrice = 1850 });
+listOfItems.Add(new AddToCart() { Id = 10, ItemName = "Affogato ", ItemPrice = 2500 });
 
-
-string[] names = { "Genesis", "mathew", "exodus", "luke", "Samuel", "Mark", "Romans", "Titus", "Peter", "Jude", "Kings", "Ezekeil", "Moses", "Sarah", "Adam", "Eve", "Esther", "Obadiah", "Lamb", "Jesus" };
-foreach (string name in names)
-{
-    Console.WriteLine(name);
-}
+AddToCart.ViewProduct(listOfItems);
