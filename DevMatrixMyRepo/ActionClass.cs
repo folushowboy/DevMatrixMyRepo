56f5d339990c612ba1291bf9de6253065646899f
Odusina Tolulope <odusinat8@gmail.com>
2026-06-30 11:52:06 +0100

    signal

---
+++ DevMatrixMyRepo/ActionClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    internal class ActionClass
+    {
+       public static void ActionRequired()
+        {
+            Console.WriteLine("Do the action");
+            Console.WriteLine("Action Completed");
+        }
+    }
+}
---
+++ DevMatrixMyRepo/Signalling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevMatrixMyRepo
+{
+    public delegate void Notify(); //delegate
+    internal class Signalling
+    {
+        public event Notify ProcessCompleted; //event
+        //this is the signal
+        public void StartProcess()
+        {
+            Console.WriteLine("Start process");
+            OnprocessCompleted();
+        }
+        protected virtual void OnprocessCompleted()
+        {
+            ProcessCompleted.Invoke(); 
+            //this is raising the signal
+        }
+    }
+}
