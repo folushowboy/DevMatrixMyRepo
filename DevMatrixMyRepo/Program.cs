be818cdc837a26f6afc45f7a7e0a9923f107aabd
Odusina Tolulope <odusinat8@gmail.com>
2026-07-27 00:02:40 +0100

    fileHandling

--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,42 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-// Asynchronous Programming
-
-
-await LastProcess();
-await FirstProcess();
-//Synchronous Method
-static async Task FirstProcess()
-{
-    Console.WriteLine("Welcome to the first process");
-    Console.WriteLine("The first process is starting now......");
-    Console.WriteLine("The first process is completed.");
-}
-
-//Asynchronous Method
-static async Task LastProcess()
-{
-    Console.WriteLine("Welcome to the Last process");
-    await Task.Delay(1000);
-    Console.WriteLine("The Last process is starting now......");
-    Console.WriteLine("The Last process is completed.");
-
-    //await DateReturnProcess(); // To call an asynchronous method it has to be in an asynchronous main method
-}
-
-static async void Last() // Same thing as Task
-{
-   
-}
-//Asynchronous DataType Method
-static async Task<bool> DateReturnProcess()
-{
-    return true;
-}
-
-//Synchronous DataType Method
-static bool DataProcess()
-{
-    return true;
-
-}
+//Path
+string path = @"C:\Users\User\OneDrive\Documents\My Web Sites\vocabulary.txt";
+string senence = "Jesus loves you";
+File.WriteAllText(path, senence);
+var value = Directory.GetLastAccessTime(path);
+Console.WriteLine(value);
