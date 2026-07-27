d96a28b8f36c0a8a5f9d9ae355516f4a573bbbc6
DELL <DELL@DESKTOP-NPJ4ILO>
2026-07-27 13:03:18 +0100

    calc

--- DevMatrixMyRepo/CalculatorProject.cs
+++ DevMatrixMyRepo/CalculatorProject.cs
@@ -1,12 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DevMatrixMyRepo
 {
-    internal class CalculatorProject
+    public class CalculatorProject
     {
+        public void Add(int x, int y, int z)
+        {
+            Console.WriteLine("Enter two number to Add");
+            int result = x + y + z;
+            Console.WriteLine(result);
+        }
+
+        public static int Subtract(int x, int y, int z)
+        {
+            int ans = x - y - z;
+            return ans;
+        }
+
+        public static double Multiply(double x, double y, double z)
+        {
+            double result = x * y * z;
+            return result;
+        }
+
+        public long Divide(long x, long y, long z)
+        {
+            long ans = x / y / z;
+            return ans;
+        }
+
+        public static void Convertion()
+        {
+            var rate = 1500;
+            Console.WriteLine("What currncy would like to convert to?");
+            Console.WriteLine("1. USD -> EUR");
+            Console.WriteLine("2. N -> USD");
+            Console.WriteLine("3. USD -> GBP");
+            Console.WriteLine("4. GBP -> N");
+            Console.WriteLine("1. USD -> YEN");      
+            
+            while (true) 
+            {
+                if (!int.TryParse(Console.ReadLine(), out var option))
+                {
+                    Console.WriteLine("Invalid Input. Enter a number between 1 and 5");
+                    continue;
+                }else if (!double.TryParse(Console.ReadLine(), out var amount))
+                {
+                    Console.WriteLine("Invalid Input. Enter a number between 1 and 5");
+                    continue;
+                }
+                var result = 0;
+                switch (option)
+                {
+                    case 1:
+                        result = 1 * rate;
+                        break;
+                }
+            }
+
+           
+        }
+
     }
 }
--- DevMatrixMyRepo/Program.cs
+++ DevMatrixMyRepo/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using DevMatrixMyRepo;
 
+//Console.WriteLine("Hello, World!");
 
+//Console.WriteLine(CalculatorProject.Multiply(22, 34.3, 44));
+//Console.WriteLine(CalculatorProject.Subtract(10, 22, 33));
+//CalculatorProject calc = new CalculatorProject();
+//calc.Add(11,33,45);
+//Console.WriteLine(calc.Divide(13, 45, 6));
+CalculatorProject.Convertion();
